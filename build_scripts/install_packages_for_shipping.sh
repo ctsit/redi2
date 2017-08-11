aed487baee9f491ac614f58c738bd304bd8c5cb7
Matthew McConnell <bigmatt0211@ufl.edu>
2017-08-11 14:34:25 -0400

    switch the vagrant to pull from my repo and try to compile correctly

--- build_scripts/install_packages_for_shipping.sh
+++ build_scripts/install_packages_for_shipping.sh
@@ -1,3 +1,4 @@
 source ../venv/bin/activate
 # the disconnected environment requires particular versions of dependencies for claw
-pip3 install --target ../packages -r ../../build_scripts/frozen_deps.txt
+pip3 install --no-cache-dir --target ../packages -r ../../build_scripts/frozen_deps.txt
+deactivate
--- vagrant/vagrant/redi2_functions.sh
+++ vagrant/vagrant/redi2_functions.sh
@@ -5,7 +5,8 @@ function install_redi2() {
 
     pushd ${REDI2_USER_HOME}
     rm -rf ./redi2
-    git clone ${REDI2_REPO}
+    #git clone ${REDI2_REPO}
+    git clone -b feature_fix_vagrant ${REDI2_REPO}
     chown -R ${REDI2_USER}:${REDI2_USER} ./redi2
         pushd redi2/build_scripts
         # prepare a install2 tar
