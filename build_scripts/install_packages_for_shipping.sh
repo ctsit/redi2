916270fd00b0dbe447e95a385fd5129ebe43b772
Matthew McConnell <bigmatt0211@ufl.edu>
2017-08-18 14:27:29 -0400

    attempt to download all the dependencies of packages

--- build_scripts/install_packages_for_shipping.sh
+++ build_scripts/install_packages_for_shipping.sh
@@ -1,5 +1,8 @@
 source ../venv/bin/activate
 # the disconnected environment requires particular versions of dependencies for claw
-pip3 install --target ../packages -r ../../build_scripts/frozen_deps.txt
-
+#pip3 install --target ../packages -r ../../build_scripts/frozen_deps.txt
+for dir in `find . -type d`
+do
+    pip3 download --no-cache-dir -d ../packages -e ./dir
+done
 deactivate
