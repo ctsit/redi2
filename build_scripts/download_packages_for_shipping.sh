e22c93bd0a67af1cd0de4f474d2bd1fedf0dda25
Matthew McConnell <bigmatt0211@ufl.edu>
2017-08-18 15:52:45 -0400

    force downloading to check locally first

--- build_scripts/download_packages_for_shipping.sh
+++ build_scripts/download_packages_for_shipping.sh
@@ -1,6 +1,6 @@
 source ../venv/bin/activate
 for dir in `ls -d */`
 do
-    pip3 download --no-cache-dir -d ../packages -e ./$dir
+    pip3 download --find-links="../packages" --no-cache-dir -d ../packages -e ./$dir
 done
 deactivate
