8c395c14a64d39c5f779f755e3989b7d018c014c
Matthew McConnell <bigmatt0211@ufl.edu>
2017-08-18 15:16:19 -0400

    add back venv

--- build_scripts/package.sh
+++ build_scripts/package.sh
@@ -11,9 +11,12 @@ echo "Installing your packages in in redi2/packages"
 bash ../../build_scripts/download_packages_for_shipping.sh
 popd
 
+echo "Removing the virtualenv"
+rm -rf venv
+
 echo "Creating a tar archive for transfer"
 popd # back at the top level, need to go one more up
 cd ..
 tar -czf redi2.tar.gz redi2/
-rm -rf redi2
+
 echo "Tar created, now send it to the target system"
--- build_scripts/setup_directories.sh
+++ build_scripts/setup_directories.sh
@@ -1,2 +1,3 @@
 mkdir -p redi2/repos
 mkdir -p redi2/packages
+virtualenv -p python3 redi2/venv
