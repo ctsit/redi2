e3c29a1627ab6c5c8be71fae4f8e916c585dac73
Matthew McConnell <bigmatt0211@ufl.edu>
2017-09-05 16:12:08 -0400

    download the latest pip package for shipping

--- build_scripts/download_packages_for_shipping.sh
+++ build_scripts/download_packages_for_shipping.sh
@@ -3,4 +3,5 @@ for dir in `ls -d */`
 do
     pip3 download --find-links="../packages" --no-cache-dir -d ../packages -e ./$dir
 done
+pip3 download --find-links="../packages" --no-cache-dir -d ../packages pip
 deactivate
